6dcf59913e6c996bc4a53cba929e2a372a98b14e
mousto090 <mousto090@gmail.com>
2021-04-17 11:32:18 +0200

    Fix savane_inventory report generation

--- addons/savane_inventory/models/stock_move.py
+++ addons/savane_inventory/models/stock_move.py
@@ -1,8 +1,15 @@
+import logging
+
 from odoo import models, fields
+
+_logger = logging.getLogger(__name__)
 
 
 class StockMove(models.Model):
     """This class inherit the built-in odoo stock.move"""
     _inherit = 'stock.move'
-    color_id = fields.Many2one('savane.color', ondelete='cascade', string='Couleur', required=True)
-    brand_id = fields.Many2one('savane.brand', ondelete='cascade', string='Marque', required=True)
+    color_id = fields.Many2one('savane.color', string='Couleur', required=True)
+    brand_id = fields.Many2one('savane.brand', string='Marque', required=True)
+
+    # def action_confirm(self, cr, uid, ids, context=None):
+    #     _logger.debug(self, cr, uid, ids, context)
