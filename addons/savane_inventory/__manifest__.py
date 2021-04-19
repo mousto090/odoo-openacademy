b2adff2bcd98ee92b63f469bdaaabce0cee50980
mousto090 <mousto090@gmail.com>
2021-04-19 18:57:43 +0200

    Add models access control security to give non admin users permissions on savane_inventory_module

--- addons/savane_inventory/__manifest__.py
+++ addons/savane_inventory/__manifest__.py
@@ -25,7 +25,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         # 'views/savane.xml',
         'views/stock_move.xml',
         'reports/report_stockpicking_operations.xml',
